d1928bd391fb2bab0983dff5f3f6d27d0b170ea2
ddbrennan <ddbren@gmail.com>
2018-05-06 21:35:44 -0400

    calendar layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Calendar from './components/Calendar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Hang With Tom</p>
-        <div>Calendar</div>
+        <div id="top-nav">Hang With Tom</div>
+        <Calendar />
       </div>
     );
   }
---
+++ src/components/Calendar.js
@@ -0,0 +1,59 @@
+import React from "react"; 
+ 
+class Calendar extends React.Component { 
+ 
+  render() { 
+    return ( 
+      <div id="calendar-box">
+        <div id="month-label">MAY</div>
+        <div id="top-labels">
+          <div className="top-label">SUNDAY</div>
+          <div className="top-label">MONDAY</div>
+          <div className="top-label">TUESDAY</div>
+          <div className="top-label">WEDNESDAY</div>
+          <div className="top-label">THURSDAY</div>
+          <div className="top-label">FRIDAY</div>
+          <div className="top-label">SATURDAY</div>
+        </div>
+        <div id="calendar">
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+          <div className="day"></div>
+        </div>
+      </div>
+    ) 
+  } 
+ } 
+export default Calendar
