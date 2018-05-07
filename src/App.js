0744c35d472f06f32919936428d7599d7ab20480
ddbrennan <ddbren@gmail.com>
2018-05-06 21:14:16 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Hang With Tom</p>
+        <div>Calendar</div>
+      </div>
+    );
+  }
+}
+
+export default App;
